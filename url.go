2b415d079c80dc29756fa32aa1f31cb74e6f5ad2
Haorui Wen <haorui2019@gmail.com>
2024-02-24 17:39:10 +0100

    error handling complete

--- url.go
+++ url.go
@@ -25,7 +25,7 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-	w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+	w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Text-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
