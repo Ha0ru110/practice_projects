d02647c81a1558da860752a26a176f8abc0c6ba8
Haorui Wen <haorui2019@gmail.com>
2024-05-05 11:05:35 +0200

    final project completed

--- go-the_complete_guide-all_important_projects/section-11/frst-rest-api.go
+++ go-the_complete_guide-all_important_projects/section-11/frst-rest-api.go
@@ -1,1 +1,19 @@
-package section_11
+package main
+
+import (
+	"fmt"
+	"frstapi.com/eventorganisersystem/db"
+	"frstapi.com/eventorganisersystem/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	err := server.Run(":8080")
+	if err != nil {
+		fmt.Println("starting server failed")
+		return
+	}
+}
